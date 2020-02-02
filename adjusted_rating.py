d23cbe7e02f47ace65787cddd4eae453695671c9
Jake Redekopp <tjredekopp@google.com>
2020-02-02 14:11:30 -0500

    Get (and write to csv) offensive rating for both teams in each game.

--- adjusted_rating.py
+++ adjusted_rating.py
@@ -4,6 +4,8 @@ import os
 #source env/bin/activate
 #pip3 install -r ../basketball_reference_web_scraper/requirements.txt
 
+import csv
+from datetime import date, datetime, timedelta
 import sys
 sys.path.append("/usr/local/google/home/tjredekopp/projects/basketball_reference_web_scraper")
 
@@ -11,16 +13,44 @@ import basketball_reference_web_scraper as scraper
 from basketball_reference_web_scraper import client
 
 
-def load_schedule():
-	if os.path.exists('/tmp/sched'):
-		pass
-    #TODO: read from this
-	schedule = client.season_schedule(2020)
+def load_scores():
+  filename = 'ratings.csv'
+  if os.path.exists(filename):
+    f = open(filename)
+    reader = csv.reader(f)
+    rows = []
+    for row in reader:
+      rows.append(row)
+    return rows
+
+  # First day of season is Oct 22, 2019.
+  d = date(2019, 10, 22)
+  end_date = date(2019, 10, 22) + timedelta(days=2)
+#  end_date = datetime.now() - timedelta(days=1)
+
+  games = []
+  while d < end_date:
+    print('Loading scores for %s.' % d)
+    scores = client.team_box_scores(d.day, d.month, d.year)
+    if len(scores) % 2 != 0:
+      raise Exception('Odd number of scores on %s!' % d)
+    i = 0
+    while i < len(scores):
+      away_stats = scores[i]
+      home_stats = scores[i+1]
+      games.append([away_stats['team'].name, away_stats['offensive_rating'],
+                    home_stats['team'].name, home_stats['offensive_rating']])
+      i += 2
+    d += timedelta(days=1)
+
+  f = open('ratings.csv', 'w')
+  w = csv.writer(f)
+  for game in games:
+    w.writerow(game)
+
+  f.close()
+  return games
 
 
-
-#schedule = load_schedule()
-# First day of season is Oct 22, 2019.
-
-scores = client.team_box_scores(22, 10, 2019)
-print(scores[0])
+games = load_scores()
+print(games)
